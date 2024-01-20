3b633ecea6d84fb6aa20a08de2012115a414fa44
wusskk <shikaicang@163.com>
2024-01-20 11:06:42 +0800

    change to AsyncReadExt and AsyncWriteExt trait

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,12 @@
 use anyhow::{anyhow, Result};
 use tokio::{
-    io,
-    net::{TcpListener, TcpStream},
+    io::{AsyncReadExt, AsyncWriteExt},
+    // join,
+    net::{
+        tcp::{ReadHalf, WriteHalf},
+        TcpListener, TcpStream,
+    },
+    time::{sleep, Duration},
 };
 
 use log::{error, info};
@@ -14,8 +19,6 @@ async fn main() -> Result<()> {
     let mut id = 0;
     loop {
         let (client, _) = listener.accept().await?;
-        // 为每一条连接都生成一个新的任务，
-        // `client` 的所有权将被移动到新的任务中，并在那里进行处理
         tokio::spawn(async move {
             if let Err(e) = handle_connection(client, id).await {
                 error!("error while handling stream:\n{}", e);
@@ -25,12 +28,10 @@ async fn main() -> Result<()> {
     }
 }
 
-async fn handle_connection(mut client: TcpStream, id: u64) -> Result<()> {
+async fn handle_connection(mut client: TcpStream, id: usize) -> Result<()> {
     let mut buf = [0; 4096];
 
-    client.readable().await?;
-
-    let n = match client.try_read(&mut buf) {
+    let n = match client.read(&mut buf).await {
         Ok(0) => return Err(anyhow!("no data in the first request form client!")),
         Ok(n) => {
             info!("{}: received {n} bytes from the request", id);
@@ -54,9 +55,10 @@ async fn handle_connection(mut client: TcpStream, id: u64) -> Result<()> {
             Err(e) => return Err(anyhow!("error while connect to server:\n{}", e)),
         };
 
-        client.writable().await?;
-
-        match client.try_write(b"HTTP/1.1 200 Connection Established\r\n\r\n") {
+        match client
+            .write(b"HTTP/1.1 200 Connection Established\r\n\r\n")
+            .await
+        {
             Ok(_) => (),
             Err(e) => return Err(anyhow!("error while write data to client:\n{}", e)),
         };
@@ -69,9 +71,7 @@ async fn handle_connection(mut client: TcpStream, id: u64) -> Result<()> {
             Err(e) => return Err(anyhow!("error while connect to server:\n{}", e)),
         };
 
-        server.writable().await?;
-
-        match server.try_write(&buf) {
+        match server.write(&buf).await {
             Ok(_) => (),
             Err(e) => return Err(anyhow!("error while write data to client:\n{}", e)),
         };
@@ -80,70 +80,117 @@ async fn handle_connection(mut client: TcpStream, id: u64) -> Result<()> {
     let (mut client_reader, mut client_writer) = client.split();
     let (mut server_reader, mut server_writer) = server.split();
 
-    tokio::try_join!(
-        io::copy(&mut client_reader, &mut server_writer),
-        io::copy(&mut server_reader, &mut client_writer)
-    )?;
+    let mut nbytes_client: usize = 0;
+    let mut nbytes_server: usize = 0;
+
+    let mut client_buf: [u8; 4096] = [0; 4096];
+    let mut server_buf: [u8; 4096] = [0; 4096];
+    //(nbytes_client, client_reader, server_writer, client_buf),
+    // (nbytes_server, server_reader, client_writer, server_buf),
+    loop {
+        // let (a, b) = join!(
+        //     client_to_server(client_reader, server_writer, nbytes_client, client_buf, id),
+        //     server_to_client(server_reader, client_writer, nbytes_server, server_buf, id)
+        // );
+
+        // (nbytes_client, client_reader, server_writer, client_buf) = match a {
+        //     Ok(s) => s,
+        //     Err(e) => return Err(anyhow!("error while client to server:\n{}", e)),
+        // };
+
+        // (nbytes_server, server_reader, client_writer, server_buf) = match b {
+        //     Ok(s) => s,
+        //     Err(e) => return Err(anyhow!("error while server to client:\n{}", e)),
+        // };
+
+        (nbytes_client, client_reader, server_writer, client_buf) =
+            client_to_server(client_reader, server_writer, nbytes_client, client_buf, id)
+                .await
+                .unwrap();
+        (nbytes_server, server_reader, client_writer, server_buf) =
+            server_to_client(server_reader, client_writer, nbytes_server, server_buf, id)
+                .await
+                .unwrap();
+
+        if nbytes_client == 5000 || nbytes_server == 5000 {
+            info!("Thread {id} finish!");
+            break;
+        }
+        sleep(Duration::from_millis(1)).await;
+    }
 
     Ok(())
-    // let mut nbytes_client: usize = 0;
-    // let mut nbytes_server: usize = 0;
+}
 
-    // let mut client_buf: [u8; 4096] = [0; 4096];
-    // let mut server_buf: [u8; 4096] = [0; 4096];
+async fn client_to_server<'a>(
+    mut client: ReadHalf<'a>,
+    mut server: WriteHalf<'a>,
+    mut nbytes_client: usize,
+    mut client_buf: [u8; 4096],
+    id: usize,
+) -> Result<(usize, ReadHalf<'a>, WriteHalf<'a>, [u8; 4096])> {
+    if nbytes_client == 0 {
+        nbytes_client = match client.read(&mut client_buf).await {
+            Ok(0) => {
+                info!("{}: no data from client", id);
+                return Ok((5000, client, server, client_buf));
+            }
+            Ok(n) => n,
+            Err(e) => return Err(anyhow!("error while read from client:\n{}", e)),
+        }
+    }
+    info!("{}: received {} data from client:", id, nbytes_client);
 
-    // loop {
-    //     if nbytes_client == 0 {
-    //         // client.readable().await?;
-    //         nbytes_client = match client.try_read(&mut client_buf) {
-    //             Ok(0) => {
-    //                 info!("{}: no data from client", id);
-    //                 return Ok(());
-    //             }
-    //             Ok(n) => n,
-    //             Err(ref e) if e.kind() == ErrorKind::WouldBlock => 0,
-    //             Err(e) => return Err(anyhow!("error while read from client:\n{}", e)),
-    //         }
-    //     }
-    //     // let req = String::from_utf8_lossy(&client_buf[..n]);
-    //     // info!("{}: received data from client:\n{}", id, req);
+    if nbytes_client > 0 {
+        match server.write(&mut client_buf[..nbytes_client]).await {
+            Ok(0) => {
+                info!("{id}: write to sever 0 bytes");
+                return Ok((5000, client, server, client_buf));
+            }
+            Ok(n) if n == nbytes_client => nbytes_client = 0,
+            Ok(_) => {
+                info!("{id}: write to sever more than n bytes");
+                return Ok((5000, client, server, client_buf));
+            }
+            Err(e) => return Err(anyhow!("error while write to server:\n{}", e)),
+        }
+    }
 
-    //     if nbytes_client > 0 {
-    //         server.writable().await?;
-    //         match server.try_write(&mut client_buf[..nbytes_client]) {
-    //             Ok(0) => return Ok(()),
-    //             Ok(n) if n == nbytes_client => nbytes_client = 0,
-    //             Ok(_) => return Ok(()),
-    //             Err(ref e) if e.kind() == ErrorKind::WouldBlock => (),
-    //             Err(e) => return Err(anyhow!("error while write to server:\n{}", e)),
-    //         }
-    //     }
+    Ok((nbytes_client, client, server, client_buf))
+}
 
-    //     if nbytes_server == 0 {
-    //         // server.readable().await?;
-    //         nbytes_server = match server.try_read(&mut server_buf) {
-    //             Ok(0) => {
-    //                 info!("{}: no data from client", id);
-    //                 return Ok(());
-    //             }
-    //             Ok(n) => n,
-    //             Err(ref e) if e.kind() == ErrorKind::WouldBlock => 0,
-    //             Err(e) => return Err(anyhow!("error while read from server:\n{}", e)),
-    //         }
-    //     }
-    //     // let req = String::from_utf8_lossy(&server_buf[..n]);
-    //     // info!("{}: received data from server:\n{}", id, req);
+async fn server_to_client<'a>(
+    mut server: ReadHalf<'a>,
+    mut client: WriteHalf<'a>,
+    mut nbytes_server: usize,
+    mut server_buf: [u8; 4096],
+    id: usize,
+) -> Result<(usize, ReadHalf<'a>, WriteHalf<'a>, [u8; 4096])> {
+    if nbytes_server == 0 {
+        nbytes_server = match server.read(&mut server_buf).await {
+            Ok(0) => {
+                info!("{}: no data from client", id);
+                return Ok((5000, server, client, server_buf));
+            }
+            Ok(n) => n,
+            Err(e) => return Err(anyhow!("error while read from server:\n{}", e)),
+        }
+    }
+    info!("{}: received {} data from server:", id, nbytes_server);
 
-    //     if nbytes_server > 0 {
-    //         client.writable().await?;
-    //         match client.try_write(&mut server_buf[..nbytes_server]) {
-    //             Ok(0) => return Ok(()),
-    //             Ok(n) if n == nbytes_server => nbytes_server = 0,
-    //             Ok(_) => return Ok(()),
-    //             Err(ref e) if e.kind() == ErrorKind::WouldBlock => (),
-    //             Err(e) => return Err(anyhow!("error while write to client:\n{}", e)),
-    //         }
-    //     }
-    //     sleep(Duration::from_millis(1)).await;
-    // }
+    if nbytes_server > 0 {
+        match client.write(&mut server_buf[..nbytes_server]).await {
+            Ok(0) => {
+                info!("{id}: write to client 0 bytes");
+                return Ok((5000, server, client, server_buf));
+            }
+            Ok(n) if n == nbytes_server => nbytes_server = 0,
+            Ok(_) => {
+                info!("{id}: write to client more than n bytes");
+                return Ok((5000, server, client, server_buf));
+            }
+            Err(e) => return Err(anyhow!("error while write to client:\n{}", e)),
+        }
+    }
+    Ok((nbytes_server, server, client, server_buf))
 }
